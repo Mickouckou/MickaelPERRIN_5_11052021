d54200f0afdfa8eedf846b92b04f61a51a542a62
Mickaël PERRIN <mickouckou@gmail.com>
2021-05-18 10:02:08 +0200

    Pages Index et détail fonctionnelles et optimisées

--- JavaScript/detail.js
+++ JavaScript/detail.js
@@ -1,24 +1,20 @@
-const nomOurs = obtenirParametre("name");
+const idOurs = obtenirParametre("id");
 
-fetch("http://localhost:3000/api/teddies")
+fetch("http://localhost:3000/api/teddies/" + idOurs)
   .then(function(res) {
     if (res.ok) {
       return res.json();
     }
   })
   .then(function(detailPeluche) {
-    for (let i in detailPeluche) {
-      if (nomOurs == detailPeluche[i].name){
-        //Personnalisation de la balise H1
-        document.getElementById("h1").textContent = "Ours en peluche : " + nomOurs;
-        //Affichage du détail de l'ours en peluche
-        carteDetail(detailPeluche[i]);
-        }
-      }
+    //Personnalisation de la balise H1
+    document.getElementById("h1").textContent = "Ours en peluche : " + detailPeluche.name;
+    //Affichage du détail de l'ours en peluche
+    carteDetail(detailPeluche);
     })
   .catch(error => alert("Erreur : " + error));
 
-//Récupération du nom de l'ours sur la page de détail
+//Récupération de l'id de l'ours sur la page de détail
 function obtenirParametre (sVar) {
   return unescape(window.location.search.replace(new RegExp("^(?:.*[&\\?]" + escape(sVar).replace(/[\.\+\*]/g, "\\$&") + "(?:\\=([^&]*))?)?.*$", "i"), "$1"));
   }
@@ -29,15 +25,11 @@ function carteDetail(tableau){
   let elementMain = document.getElementById("detailOurs");
   elementMain.appendChild(nouvelleDiv);
   nouvelleDiv.classList.add("col-lg-6");
-  nouvelleDiv.innerHTML="<div class=\"card mb-4 mb-lg-0 border-primary shadow\"><img class=\"card-img-top\" src=\"" + tableau.imageUrl + "\" alt=\"Ours" + tableau.name + "><div class=\"card-body\"><h5 class=\"card-title\">" + tableau.name + " " + affichagePrix(tableau.price) + "</h5><p class=\"card-text\">" + tableau.description + "</p><label for=\"choixCouleur\">Choix de couleur : </label><select name=\"choixCouleur\" id=\"colorChoice\"></select><a href=\"panier.html\" class=\"btn btn-primary\">Ajouter au panier</a></div></div>";
+  nouvelleDiv.innerHTML="<div class=\"card mb-4 mb-lg-0 border-primary shadow\"><img class=\"card-img-top\" src=\"" + tableau.imageUrl + "\" alt=\"Ours" + tableau.name + "><div class=\"card-body\"><h5 class=\"card-title\">" + tableau.name + " " + new Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR' }).format(tableau.price/100) + "</h5><p class=\"card-text\">" + tableau.description + "</p><label for=\"choixCouleur\">Choix de couleur : </label><select name=\"choixCouleur\" id=\"colorChoice\"></select><a href=\"panier.html\" class=\"btn btn-primary\">Ajouter au panier</a></div></div>";
   let choixCouleur = document.getElementById("colorChoice");
   let listeCouleurs ="";
   for (let i in tableau.colors) {
     listeCouleurs+="<option value=\"" + tableau.colors[i] + "\">" + tableau.colors[i] + "</option>";
     }
-    choixCouleur.innerHTML=listeCouleurs;
-  }
-
-function affichagePrix (prix){
-  return prix /100 + " €";
+  choixCouleur.innerHTML=listeCouleurs;
   }
--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,5 +1,3 @@
-//import affichageCarte from './function.js';
-
 fetch("http://localhost:3000/api/teddies")
   .then(function(res) {
     if (res.ok) {
@@ -13,21 +11,16 @@ fetch("http://localhost:3000/api/teddies")
     })
   .catch(error => alert("Erreur : " + error));
 
-//Affiche correctement le prix renvoyé par l'API
-function affichagePrix (prix){
-  /*const prixCorrect = prix / 100;
-  return prixCorrect + " €";*/
-  return prix /100 + " €";
-  }
-
 //Affichage des cartes produits sur la page Index du site
 function affichageCarte(tableau){
   const nouvelleDiv = document.createElement("div");
   let elementMain = document.getElementById("affichage");
   elementMain.appendChild(nouvelleDiv);
-  nouvelleDiv.classList.add("col-12");
-  nouvelleDiv.classList.add("col-md-6");
-  nouvelleDiv.classList.add("col-lg-4");
-  nouvelleDiv.classList.add("mt-5");
-  nouvelleDiv.innerHTML="<div class=\"card mb-4 mb-lg-0 border-primary shadow\"><img class=\"card-img-top\" src=\"" + tableau.imageUrl + "\" alt=\"Ours" + tableau.name + "><div class=\"card-body\"><h5 class=\"card-title\">" + tableau.name + " " + affichagePrix(tableau.price) + "</h5><p class=\"card-text\">" + tableau.description + "</p><a href=\"detail.html?name=" + tableau.name + "\" class=\"btn btn-primary stretched-link\">Détails Ours " + tableau.name + "</a></div></div>";
+  nouvelleDiv.classList.add("col-12", "col-md-6", "col-lg-4", "mt-5");
+  nouvelleDiv.innerHTML="<div class=\"card mb-4 mb-lg-0 border-primary shadow\"><img class=\"card-img-top\" src=\"" + tableau.imageUrl + "\" alt=\"Ours" + tableau.name + "><div class=\"card-body\"><h5 class=\"card-title\">" + tableau.name + " " + new Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR' }).format(tableau.price/100) + "</h5><p class=\"card-text\">" + tableau.description + "</p><a href=\"detail.html?id=" + tableau._id + "\" class=\"btn btn-primary stretched-link\">Détails Ours " + tableau.name + "</a></div></div>";
   }
+
+/*//Affiche correctement le prix renvoyé par l'API
+function affichagePrix (prix){
+  return prix /100 + " €";
+  }*/  
